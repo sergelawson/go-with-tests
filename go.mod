261d5f36221ee5bf78db1b3d27dca89f321f206d
Serge Lawson <iamsergelawson@gmail.com>
2025-04-25 02:59:03 +0000

    feat: iteration

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sergelawson/go-with-tests
+
+go 1.23.4
---
+++ iteration/repeat_test.go
@@ -0,0 +1,45 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+
+	t.Run("repeat a character", func(t *testing.T) {
+
+		repeated := Repeat("a", 1)
+
+		expected := "a"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat 5 characters", func(t *testing.T) {
+
+		repeated := Repeat("y", 5)
+
+		expected := "yyyyy"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat(){
+	repeat := Repeat("a", 5)
+	fmt.Println(repeat)
+	// Output: aaaaa
+
+
+}
